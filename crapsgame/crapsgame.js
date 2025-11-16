c147cdf561ceaa6be04d1b6966975f6ceeb8d302
Barakat Egberinde <barakategberinde@gmail.com>
2025-11-16 09:40:09 +0100

    Added username validation to craps

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -5,9 +5,24 @@ const crapsMainSection = "craps-main-section"
 
 function registerCrapsPlayer() {
     let crapsUsername = document.getElementById(crapsUsernameInput).value 
-    alert("Got: " + crapsUsername)
+    //alert("Got: " + crapsUsername)
+
+
+    //Username validation check
+    let firstCharIsDigitRegex = /^[0-9]|[^a-zA-Z0-9_]/g
+    let matchedInvalidChar = firstCharIsDigitRegex.test(crapsUsername)
+    
+    let usernameIsNotValid = crapsUsername.length < 5
+
+    if(usernameIsNotValid || matchedInvalidChar){
+        alert("Username must be at least five characters long, alphanumeric only, no spaces, and cannot start with a number!")
+    }
+    else{
     removeRegistrctionPane ()
     showMainGameSection ()
+    }
+
+    
 }
 
 function removeRegistrctionPane (){
