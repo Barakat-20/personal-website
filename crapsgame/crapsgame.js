925be5ea1954ddede599250608f94d32ded7ef25
Barakat Egberinde <barakategberinde@gmail.com>
2025-11-27 16:28:03 +0100

    Implemented round finish grid

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -20,6 +20,9 @@ const crapsStartsRounds = "craps-starts-rounds"
 const crapsUserBetAmount= "craps-user-bet-amount"
 const crapsRollDiceButton = "craps-roll-dice-button"
 const crapsRollDiceAnimationContainer = "craps-roll-dice-animation-container"
+const crapsBettingGridContainer = "craps-betting-grid-container"
+const crapsRoundFinishGridContainer = "craps-round-finish-grid-container"
+const crapsRoundFinishMessage = "craps-round-finish-message"
 
 //In-games variables
 let currentMoney = startingMoney
@@ -60,6 +63,7 @@ function showMainGameSection (){
 }
 
 function setupFirstRound (){
+    document.getElementById(crapsRoundFinishGridContainer).style.display = "none"
     document.getElementById(crapsStartsUsername).innerHTML = crapsUsername
 
     setMoney (startingMoney)
@@ -129,7 +133,7 @@ function formatDiceScale () {
 }
 
 function delayedProcessDiceResult (diceResult) {
-    setTimeout(function() {processDiceResult(diceResult) }, 1000)
+    setTimeout(function() {processDiceResult(diceResult) }, 1800)
 }
 function processDiceResult (diceResult) {
     const sum = diceResult.reduce((partialSum, a) => partialSum + a, 0)
@@ -138,11 +142,18 @@ function processDiceResult (diceResult) {
         diceSumResult = bets.odd
     }
     setRounds(currentRounds + 1)
+    let roundFinishMessage = ""
     if (diceSumResult === currentBet) {
-        //alert("YOU WIN")
+        roundFinishMessage = "YOU WIN!"
         setMoney(currentMoney + currentBetAmount)
     }else {
-        //alert("YOU LOSE")
+        roundFinishMessage = "YOU LOSE😂"
         setMoney(currentMoney - currentBetAmount)
     }
+    if (currentMoney === 0) {
+        roundFinishMessage = "YOU'RE OUT!"
+    }
+    document.getElementById(crapsBettingGridContainer).style.display = "none"
+    document.getElementById(crapsRoundFinishGridContainer).style.display = "block"
+    document.getElementById(crapsRoundFinishMessage).innerHTML = roundFinishMessage
 }
