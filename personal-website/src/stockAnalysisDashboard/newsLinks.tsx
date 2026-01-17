bc29a919909a18152fb978681c837001cafbb85f
Barakat-20 <barakategberinde@gmail.com>
2026-01-17 14:59:47 +0100

    Completed news links section with ChatGPT

---
+++ personal-website/src/stockAnalysisDashboard/newsLinks.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PrimaryColor } from './stockAnalysisDashboard';
+
+interface NewsLink {
+  title: string;
+  link: string;
+}
+
+interface NewsListProps {
+  newsLinks: NewsLink[];
+}
+
+const NewsItem = styled.div`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%; 
+  margin-bottom: 10px; 
+`;
+
+const NewsLink = styled.a`
+  text-decoration: none;
+  color: ${PrimaryColor}; 
+  font-size: 12px;
+`;
+
+const ScrollableList = styled.div`
+  max-height: 80%;
+  overflow-y: auto;
+`;
+
+const NewsListContainer = styled.div`
+  max-width: 100%;
+`
+
+const NewsList: React.FC<NewsListProps> = ({ newsLinks }) => {
+  return (
+    <NewsListContainer>
+      <div style={{marginBottom: '10px', color: 'black'}}>In The News</div>
+      <ScrollableList>
+        {newsLinks.map((news) => (
+          <NewsItem>
+            <NewsLink href={news.link} title={news.title} target='_blank' rel='noopener noreferrer'>
+              {news.title}
+            </NewsLink>
+          </NewsItem>
+        ))}
+      </ScrollableList>
+    </NewsListContainer>
+  );
+};
+
+
+
+export default NewsList;
