6b9f6f2402ac3e574c71a5f7108e346dd6aaca4c
Barakat-20 <barakategberinde@gmail.com>
2026-01-18 14:41:24 +0100

    Added vite custom build steps

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './src/crapsgame/crapsgame.js',
+          dest: './src/crapsgame/'
+        },
+        {
+          src: './src/randomQuoteGenerator/randomQuoteGenerator.js',
+          dest: './src/randomQuoteGenerator/'
+        },
+        {
+          src: './src/cv/Bade_Tola_CV - Copy.pdf',
+          dest: './src/cv/'
+        },
+      ],
+    }),
+  ],
 })
